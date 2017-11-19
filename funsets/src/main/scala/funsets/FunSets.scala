46c457d20e58454c634b0178ce91b5de238b759b
Martin <koo.martin@gmail.com>
2017-11-18 21:55:05 -0500

    funsets complete up to map

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -25,25 +25,25 @@ object FunSets {
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = s ++ t
+  def union(s: Set, t: Set): Set = (x : Int) => { contains(s,x) || contains(t,x) }
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = s.intersect(t)
+  def intersect(s: Set, t: Set): Set = (x : Int) => { contains(s,x) && contains(t,x) }
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = s -- t
+    def diff(s: Set, t: Set): Set = (x : Int) => {contains(s,x) && !contains(t,x)}
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = s.filter(p)
-  
+    def filter(s: Set, p: Int => Boolean): Set = (x : Int) => { contains(s,x) && p(x) }
+    
   /*
    * The bounds for `forall` and `exists` are +/- 1000.
    */
@@ -54,18 +54,25 @@ object FunSets {
    */
     def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a == bound) true
+      else if (contains(s, a) && !p(a)) false
+      else iter(a + 1)
     }
-    iter(???)
+    iter(-bound)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
+    def exists(s: Set, p: Int => Boolean): Boolean = {
+      def iter(a: Int): Boolean = {
+        if(a == bound) false
+        else if (contains(s, a) && p(a)) true
+        else iter(a + 1)
+      }
+      iter(-bound)
+    }
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
